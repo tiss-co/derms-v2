2dd58d71d7344175157ebfd7c24af4835c46498b
Mohammadreza Khoobbakht <f69.khoobbakht@gmail.com>
2023-02-27 14:58:52 +0330

    refactor(pcs battery calculation): change `calculate_pcs_battery_consumption` based on updated input models

--- backend/tasks/utils.py
+++ backend/tasks/utils.py
@@ -8,15 +8,16 @@ from deepdiff import DeepDiff
 
 from backend.api.common import get_loads
 from backend.configs import TIMEZONE
-from backend.database import func
+from backend.database import func, session
 from backend.extensions import redis
 from backend.logger import logger
-from backend.models import Action, Activation, Alarm, Load, Result, Program
+from backend.models import Action, Activation, Alarm, Battery, Program, Result
 from backend.modules.derms import PCSBattery, settings
+from backend.serializers import ActivationSchema, BatterySchema, ProgramSchema
 from backend.utils import tznow
 
 
-def calculate_pcs_battery_consumption() -> None:
+def calculate_pcs_battery_consumption(comp_id: int, battery_id: int) -> None:
     """Calculate battery charge and discharge values
 
     Based on load values and activated programs
@@ -27,106 +28,49 @@ def calculate_pcs_battery_consumption() -> None:
         No load data found.
     """
 
-    # Extracting yesterday's latest values (to get the latest available soc)
-    last_res_of_yesterday = Result.get_by(
-        datetime=datetime.combine(
-            (tznow(TIMEZONE) - timedelta(days=1)).date(), time(23)
-        )
-    )
-
-    activated_programs_results = (
-        Activation.filter(func.date(Activation.date) == tznow(TIMEZONE).date())
-        .order_by(Activation.date)
-        .all()
-    )
-
-    activated_programs = dict()
-    for item in activated_programs_results:
-        activated_programs[item.name.lower()] = {
-            "status": item.status,
-            "activation": {"start": item.start, "end": item.end},
-        }
-
-    load_consumption = get_loads(tznow(TIMEZONE).date())
+    load_consumption = get_loads(tznow(TIMEZONE).date(), comp_id=comp_id)
     load_values = [item["value"] for item in load_consumption]
+    load_dict = {k: v for k, v in enumerate(load_values, 1)}
 
     if load_values:
-        input_loads = load_values
-        pcs_battery = PCSBattery(
-            SOC_max=int(settings.SOC_max),
-            p_max=settings.p_max,
-            SOC_available=float(
-                last_res_of_yesterday.limited_soc
-                if last_res_of_yesterday
-                else settings.SOC_max
-            ),
-            GA_START_TIME=int(
-                activated_programs.get("ga", {}).get(
-                    "activation", {}).get("start") or 0
-            )
-            + 1,
-            GA_END_TIME=int(
-                activated_programs.get("ga", {}).get(
-                    "activation", {}).get("end") or 0
-            )
-            + 1,
-            DR_START_TIME=int(
-                activated_programs.get("dr", {}).get(
-                    "activation", {}).get("start") or 0
-            )
-            + 1,
-            DR_END_TIME=int(
-                activated_programs.get("dr", {}).get(
-                    "activation", {}).get("end") or 0
-            )
-            + 1,
-            HOEP_START_TIME=int(
-                activated_programs.get("hoep", {}).get(
-                    "activation", {}).get("start")
-                or 0
-            )
-            + 1,
-            HOEP_END_TIME=int(
-                activated_programs.get("hoep", {}).get(
-                    "activation", {}).get("end") or 0
-            )
-            + 1,
-            GA_STATUS=int(activated_programs.get(
-                "ga", {}).get("status", False)),
-            DR_STATUS=int(activated_programs.get(
-                "dr", {}).get("status", False)),
-            HOEP_STATUS=int(activated_programs.get(
-                "hoep", {}).get("status", False)),
-            Load=input_loads,
-            CHARGING_START_TIME1=settings.CHARGING_START_TIME1,
-            CHARGING_END_TIME1=settings.CHARGING_END_TIME1,
-            CHARGING_START_TIME2=settings.CHARGING_START_TIME2,
-            CHARGING_END_TIME2=settings.CHARGING_END_TIME2,
-            feeder_max=settings.feeder_max,
-            charging_margin=settings.charging_margin,
-            charge_max=settings.charge_max,
+        program_activation_obj = (
+            session.query(Program, Activation)
+            .filter(Program.id == Activation.program_id)
+            .filter(Program.battery_id == battery_id, Activation.date == tznow(TIMEZONE).date())
+            .all()
         )
 
-        pcs_battery.check_program_activation()
+        program_activation_res = [
+            {
+                **ActivationSchema().dump(item.Activation),
+                **ProgramSchema().dump(item.Program),
+            }
+            for item in program_activation_obj
+        ]
+
+        battery_features = dict(BatterySchema().dump(Battery.get_by(id=battery_id)))
+        battery_features["soc_available"] = 1000
+
+        pcs_battery = PCSBattery(load=load_dict, battery_features=battery_features, programs=program_activation_res)
+
+        pcs_battery.calculate_pcs_battery_consumption()
+
+        p_discharge = np.array(list(pcs_battery.model.Pbdch.get_values().values()))
+        p_charge = np.array(list(pcs_battery.model.Pbch.get_values().values()))
 
         charging_status = np.zeros(24, dtype="int32")
-        charging_status[pcs_battery.p_discharge != 0] = -1
-        charging_status[pcs_battery.p_charge != 0] = 1
+        charging_status[p_discharge != 0] = -1
+        charging_status[p_charge != 0] = 1
         charging_status = charging_status.tolist()
 
-        charging_modes = pcs_battery.daily_activity_hours().tolist()
-        power = np.abs(
-            (pcs_battery.p_charge + pcs_battery.p_discharge).round(4)
-        ).tolist()
-        limited_soc = np.abs(pcs_battery.soc.round(4)).tolist()
+        power = np.abs((p_charge + p_discharge).round(4)).tolist()
+        soc = np.array(list(pcs_battery.model.soc.get_values().values())).round(4).tolist()
 
-        utility_power = [
-            (a * b) + c for a, b, c in zip(charging_status, power, input_loads)
-        ]
+        utility_power = [(a * b) + c for a, b, c in zip(charging_status, power, load_values)]
 
         # Get all the results for today
         today_results = (
-            Result.filter(func.date(Result.datetime) == tznow(TIMEZONE).date())
+            Result.filter(func.date(Result.datetime) == tznow(TIMEZONE).date(), Result.battery_id == battery_id)
             .order_by(Result.datetime)
             .all()
         )
@@ -134,44 +78,28 @@ def calculate_pcs_battery_consumption() -> None:
         # Check if is not first time to save results to DB
         if today_results:
             charging_status_old = [rs.charging_status for rs in today_results]
-            charging_mode_old = [rs.charging_mode for rs in today_results]
             power_old = [rs.power for rs in today_results]
-            limited_soc_old = [rs.limited_soc for rs in today_results]
+            soc_old = [rs.soc for rs in today_results]
             utility_power_old = [rs.utility_power for rs in today_results]
 
             # Update the new calculated results with the previous results
-            charging_status[: tznow(TIMEZONE).hour + 1] = charging_status_old[
-                : tznow(TIMEZONE).hour + 1
-            ]
-            charging_modes[: tznow(TIMEZONE).hour + 1] = charging_mode_old[
-                : tznow(TIMEZONE).hour + 1
-            ]
-            power[: tznow(TIMEZONE).hour +
-                  1] = power_old[: tznow(TIMEZONE).hour + 1]
-            limited_soc[: tznow(TIMEZONE).hour + 1] = limited_soc_old[
-                : tznow(TIMEZONE).hour + 1
-            ]
-            utility_power[: tznow(TIMEZONE).hour + 1] = utility_power_old[
-                : tznow(TIMEZONE).hour + 1
-            ]
-
-        real_soc = [x + (settings.BATTERY_CAPACITY * 0.1) for x in limited_soc]
+            charging_status[: tznow(TIMEZONE).hour + 1] = charging_status_old[: tznow(TIMEZONE).hour + 1]
+            power[: tznow(TIMEZONE).hour + 1] = power_old[: tznow(TIMEZONE).hour + 1]
+            soc[: tznow(TIMEZONE).hour + 1] = soc_old[: tznow(TIMEZONE).hour + 1]
+            utility_power[: tznow(TIMEZONE).hour + 1] = utility_power_old[: tznow(TIMEZONE).hour + 1]
 
         # Save results for the first time
         for i in range(24):
             res = Result.get_or_create(
-                datetime=datetime.combine(tznow(TIMEZONE).date(), time(i, 0))
+                datetime=datetime.combine(tznow(TIMEZONE).date(), time(i, 0)), battery_id=battery_id
             )
             res.update(
                 commit=True,
                 charging_status=charging_status[i],
-                charging_mode=charging_modes[i],
                 power=power[i],
-                limited_soc=limited_soc[i],
-                real_soc=real_soc[i],
+                soc=soc[i],
                 utility_power=utility_power[i],
             )
-
     else:
         raise Exception("No load data found")
 
@@ -217,8 +145,7 @@ def get_list_diff(new_data: list, old_data: list):
     diff = DeepDiff(new_data, old_data)
 
     for k in diff.get("values_changed", []):
-        changes[int(re.findall(excluded_regex, k)[-1])
-                ] = diff["values_changed"][k]
+        changes[int(re.findall(excluded_regex, k)[-1])] = diff["values_changed"][k]
 
     return changes
 
@@ -233,9 +160,7 @@ def daily_activity_hours() -> list:
     activation_mode: list
     """
     activated_programs_results = (
-        Activation.filter(func.date(Activation.date) == tznow(TIMEZONE).date())
-        .order_by(Activation.date)
-        .all()
+        Activation.filter(func.date(Activation.date) == tznow(TIMEZONE).date()).order_by(Activation.date).all()
     )
 
     activated_programs = dict()
@@ -245,43 +170,20 @@ def daily_activity_hours() -> list:
             "activation": {"start": item.start, "end": item.end},
         }
 
-    ga_st = (
-        int(activated_programs.get("ga", {}).get(
-            "activation", {}).get("start") or 0)
-        + 1
-    )
-    ga_et = (
-        int(activated_programs.get("ga", {}).get(
-            "activation", {}).get("end") or 0) + 1
-    )
-    dr_st = (
-        int(activated_programs.get("dr", {}).get(
-            "activation", {}).get("start") or 0)
-        + 1
-    )
-    dr_et = (
-        int(activated_programs.get("dr", {}).get(
-            "activation", {}).get("end") or 0) + 1
-    )
-    hoep_st = (
-        int(activated_programs.get("hoep", {}).get(
-            "activation", {}).get("start") or 0)
-        + 1
-    )
-    hoep_et = (
-        int(activated_programs.get("hoep", {}).get(
-            "activation", {}).get("end") or 0)
-        + 1
-    )
+    ga_st = int(activated_programs.get("ga", {}).get("activation", {}).get("start") or 0) + 1
+    ga_et = int(activated_programs.get("ga", {}).get("activation", {}).get("end") or 0) + 1
+    dr_st = int(activated_programs.get("dr", {}).get("activation", {}).get("start") or 0) + 1
+    dr_et = int(activated_programs.get("dr", {}).get("activation", {}).get("end") or 0) + 1
+    hoep_st = int(activated_programs.get("hoep", {}).get("activation", {}).get("start") or 0) + 1
+    hoep_et = int(activated_programs.get("hoep", {}).get("activation", {}).get("end") or 0) + 1
     ga_status = int(activated_programs.get("ga", {}).get("status", False))
     dr_status = int(activated_programs.get("dr", {}).get("status", False))
     hoep_status = int(activated_programs.get("hoep", {}).get("status", False))
 
-    ga_activation, dr_activation, hoep_activation = np.zeros(
-        (3, 24), dtype="int64")
-    ga_activation[ga_st - 1: ga_et] = 1
-    dr_activation[dr_st - 1: dr_et] = 1
-    hoep_activation[hoep_st - 1: hoep_et] = 1
+    ga_activation, dr_activation, hoep_activation = np.zeros((3, 24), dtype="int64")
+    ga_activation[ga_st - 1 : ga_et] = 1
+    dr_activation[dr_st - 1 : dr_et] = 1
+    hoep_activation[hoep_st - 1 : hoep_et] = 1
 
     activation_mode = np.zeros(24, dtype="int32")
 
@@ -309,8 +211,8 @@ def check_for_update_load(new_data: dict, old_data: dict):
     status: bool
     """
 
-    new = new_data["load"][tznow(TIMEZONE).hour + 1:]
-    old = old_data["load"][tznow(TIMEZONE).hour + 1:]
+    new = new_data["load"][tznow(TIMEZONE).hour + 1 :]
+    old = old_data["load"][tznow(TIMEZONE).hour + 1 :]
 
     if new != old:
         return True
@@ -345,16 +247,10 @@ def check_activation_program_status(program_name: str, program_data: dict) -> No
             # calculate_pcs_battery_consumption()
 
         if check_status_changes(changes) or (
-            not check_status_changes(changes)
-            and check_activation_times_changes(changes)
-            and program_data.get("status")
+            not check_status_changes(changes) and check_activation_times_changes(changes) and program_data.get("status")
         ):
             for program in programs:
-
-                alarm_status = (
-                    json.loads(redis.get("need-update")
-                               ) if redis.exists("need-update") else {}
-                )
+                alarm_status = json.loads(redis.get("need-update")) if redis.exists("need-update") else {}
                 alarm_status[f"{program.battery_id}"] = 1
                 redis.set("need-update", json.dumps(alarm_status))
                 update_activation_db(program.id, program_data)
@@ -410,9 +306,7 @@ def check_activation_times_changes(changes: dict) -> bool:
 
 
 def update_activation_db(program_id: int, program_data: dict) -> None:
-    activation = Activation.get_or_create(
-        date=tznow(TIMEZONE).date(), program_id=program_id, is_manual=False
-    )
+    activation = Activation.get_or_create(date=tznow(TIMEZONE).date(), program_id=program_id, is_manual=False)
     activation.update(
         commit=True,
         status=program_data.get("status"),
